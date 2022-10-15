8d3e4e043dae92a54285ff350934a041fc60d867
Jose Perez <jmperezvzla@gmail.com>
2022-10-15 17:16:36 -0400

    Create Nosotros Template

--- libreria/views.py
+++ libreria/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+TPL = 'libreria/'
+TPL_PAGS = TPL +'paginas/'
 
 def inicio(request):
     return HttpResponse('<h1>Saludos desde la libreria</h1>')
+def nosotros(request):
+    return render(request, TPL_PAGS + 'nosotros.html')
