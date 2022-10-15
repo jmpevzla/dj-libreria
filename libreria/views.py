32de971bc4b06055ec1b15be235580a929485b3d
Jose Perez <jmperezvzla@gmail.com>
2022-10-15 18:57:25 -0400

    Add view libros & url

--- libreria/views.py
+++ libreria/views.py
@@ -4,8 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 TPL = 'libreria/'
 TPL_PAGS = TPL +'paginas/'
+TPL_LIBS = TPL + 'libros/'
 
 def inicio(request):
     return HttpResponse('<h1>Saludos desde la libreria</h1>')
 def nosotros(request):
     return render(request, TPL_PAGS + 'nosotros.html')
+
+def libros(request):
+    return render(request, TPL_LIBS + 'index.html')
