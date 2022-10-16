15859cf559aa5f6da5da35fc90de3b215990dc43
Jose Perez <jmperezvzla@gmail.com>
2022-10-15 20:31:09 -0400

    Fix ImageField in Libro (upload_to) Register admin Libro

--- libreria/models.py
+++ libreria/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Libro(models.Model):
     id = models.AutoField(primary_key = True)
     titulo = models.CharField(max_length = 100, verbose_name = 'Titulo')
-    imagen = models.ImageField(upload_to = 'imagenes/', null = True, verbose_name = 'Imagen')
+    imagen = models.ImageField(upload_to = 'media/', null = True, verbose_name = 'Imagen')
     descripcion = models.TextField(null = True, verbose_name = 'Descripcion')
