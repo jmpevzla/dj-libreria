b0eacb639a38b56cb98c2a192d9292e020b93a77
Jose Perez <jmperezvzla@gmail.com>
2022-10-16 00:15:04 -0400

    Update form.html with form helper

--- libreria/models.py
+++ libreria/models.py
@@ -5,7 +5,7 @@ class Libro(models.Model):
     id = models.AutoField(primary_key = True)
     titulo = models.CharField(max_length = 100, verbose_name = 'Titulo')
     imagen = models.ImageField(upload_to = 'media/', null = True, verbose_name = 'Imagen')
-    descripcion = models.TextField(null = True, verbose_name = 'Descripcion')
+    descripcion = models.TextField(null = True, verbose_name = 'Descripción')
 
     def __str__(self) -> str:
         return self.titulo
