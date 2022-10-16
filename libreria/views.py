586e299a4e0b9673b4b46ebcbeb79c39dae24e45
Jose Perez <jmperezvzla@gmail.com>
2022-10-16 00:30:26 -0400

    Save formulario in crear view LibroForm with request.FILES redirect to libros

--- libreria/views.py
+++ libreria/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Libro
 from .forms import LibroForm
 
@@ -17,7 +17,11 @@ def libros(request):
     libros = Libro.objects.all()
     return render(request, TPL_LIBS + 'index.html', { 'libros': libros })
 def crear(request):
-    formulario = LibroForm(request.POST or None)
+    formulario = LibroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('libros')
+    # print(formulario.fields.get('titulo').label)
     return render(request, TPL_LIBS + 'crear.html', { 'formulario': formulario })
 def editar(request):
     return render(request, TPL_LIBS + 'editar.html')
